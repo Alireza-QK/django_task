4c23c9b4f2a4439685bf1374b59d77fe86fcc96c
Alireza_QK <Alireza.QK.5@gmail.com>
2021-05-07 23:57:51 +0430

    install postgres & setting

--- task/task/settings.py
+++ task/task/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-task-db',
+        'USER': 'postgres',
+        'PASSWORD': 'A123456b',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
