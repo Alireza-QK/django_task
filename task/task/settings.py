aa90f503d07826179299e0b577c6c77228bfc154
Alireza_QK <Alireza.QK.5@gmail.com>
2021-05-08 00:06:22 +0430

    add app task

--- task/task/settings.py
+++ task/task/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Our app
+    'task_app.apps.TaskAppConfig'
 ]
 
 MIDDLEWARE = [
