00b323cd95f57fb9a1fce86fdeaf7ee2aaa94bd9
Alireza_QK <Alireza.QK.5@gmail.com>
2021-05-08 00:06:47 +0430

    Make model task

--- task/task_app/models.py
+++ task/task_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Task(models.Model):
+	title = models.CharField(max_length=254, verbose_name='Title')
+	status = models.BooleanField(default=False, verbose_name='Status task')
+	user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		db_table = 'task_tbl'
+		verbose_name = 'task'
+		verbose_name_plural = 'tasks'
+		
