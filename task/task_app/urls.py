873eb9d1a81c5e35cad657ad4aff3eeab5b01493
Alireza_QK <Alireza.QK.5@gmail.com>
2021-05-09 12:46:10 +0430

    CRUD change to class view in django

--- task/task_app/urls.py
+++ task/task_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import homePage, CreateTaskPage, deleteTask, EditTaskPage
+from .views import (homePage, CreateTaskView, DeleteTaskView, UpdateTaskView)
 
 app_name = 'task'
 
 urlpatterns = [
 	path('', homePage, name='home'),
-	path('create-task', CreateTaskPage, name='create-task'),
-	path('delete-task/<int:pk>', deleteTask, name='delete-task'),
-	path('edit-task/<int:pk>', EditTaskPage, name='edit-task'),
+	path('create-task', CreateTaskView.as_view(), name='create-task'),
+	path('delete-task/<int:pk>', DeleteTaskView.as_view(), name='delete-task'),
+	path('edit-task/<int:pk>', UpdateTaskView.as_view(), name='edit-task'),
 ]
--- task/task_app/views.py
+++ task/task_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http.response import HttpResponse
 from django.contrib import messages
-from django.urls import reverse
-from django.views.generic.edit import UpdateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from .models import Task
 from .forms import TaskForm
 
@@ -14,46 +14,21 @@ def homePage(request):
 	return render(request, 'task_app/index.html', context=context)
 
 
-def CreateTaskPage(request):
-
-	form = ''
-	if request.method == 'POST':
-		form = request.POST
-
-		if form['title_task']:
-			title = form['title_task']
-			Task.objects.create(title=title)
-			return redirect(reverse('task:home'))
-		else:
-			messages.error(request, "title can't empty", 'danger')
-
-	return render(request, 'task_app/create-task.html', context={})
+class CreateTaskView(CreateView):
+	template_name = 'task_app/create-task.html'
+	model = Task
+	form_class = TaskForm
+	success_url = '/'
 
 
-def deleteTask(request, pk):
-	
-	if pk:
-		try:
-
-			Task.objects.get(pk=pk).delete()
-			return redirect(reverse('task:home'))
-		except Task.DoesNotExist:
-			redirect(reverse('task:home'))	
-
-	return redirect(reverse('task:home'))
+class UpdateTaskView(UpdateView):
+	template_name = 'task_app/edit-task.html'
+	model = Task
+	fields = ['title']
+	success_url = '/'
 
 
-def EditTaskPage(request, pk):
-
-	context ={}
-	obj = get_object_or_404(Task, pk = pk)
-	form = TaskForm(request.POST or None, instance=obj or None)
-
-	if form.is_valid():
-		# save the form data to model
-		form.save()
-		return redirect(reverse('task:home'))
-	
-	context['form'] = form
-	return render(request, 'task_app/edit-task.html', context)
-
+class DeleteTaskView(DeleteView):
+	model = Task
+	template_name = 'task_app/delete-task.html'
+	success_url = reverse_lazy('task:home')
