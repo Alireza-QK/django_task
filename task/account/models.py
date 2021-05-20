e387cb1540a2d90074c92f982f5f9cfa931b01ee
Alireza_QK <Alireza.QK.5@gmail.com>
2021-05-20 10:37:34 +0430

    add account model & authenticated

--- task/account/models.py
+++ task/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email address', unique=True)
+
+    def __str__(self):
+        return self.username
--- task/account/views.py
+++ task/account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
+
 def profile(request):
-    return httpResponse('My profile')
+    return HttpResponse('My profile')
